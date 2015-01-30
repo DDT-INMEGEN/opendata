1c98fa04a8b389c06b435a650828afca4f291d09
Rodrigo Garcia <absorto@sdf.org>
2015-01-30 15:12:33 -0600

    form on a pop up
    when user clicks on download link up pops the registry form

--- static/download.js
+++ static/download.js
@@ -1,47 +1,63 @@
-var path = 'prueba';
+function register_download(path) {
+    w2popup.open({
+        title   : 'Popup',
+        width   : 400,
+        height  : 400,
+        body    : '<div style="margin-bottom: 1.3em; margin-top: 1em">Porfa regístrese para descargar <em>' + path + '</em></div><div id="download_form"></div>',
+        onOpen  : function (event) {
+            event.onComplete = function () {
 
-$(function () {
-    $('#download_form').w2form({ 
-        name   : 'download_form',
-        fields : [
-            { name: 'first_name', type: 'text', required: true, html: {caption: 'Nombre' } },
-            { name: 'last_name',  type: 'text', required: true, html: {caption: 'Apellidos' } },
-            { name: 'email',  type: 'email', required: true, html: {caption: 'Correo Electrónico' } },
-            { name: 'email_repeat',  type: 'email', required: true, html: {caption: 'Confirmar Correo' } },
-            { name: 'comments',   type: 'textarea', html: {caption: 'Comentarios' }}
-        ],
-        actions: {
-            reset: function () {
-                this.clear();
-            },
-
-            save: function () {
-                if (this.record['email'] == this.record['email_repeat']) {
-                    this.record['path'] = path
-                    $.ajax({
-                        type: "POST",
-                        url: "/register",
-                        data: JSON.stringify( this.record ),
-                        contentType: "application/json; charset=utf-8",
-                        dataType: "json",
-                        error: function(errMsg) {
-                            this.error("AJAX error" + errMsg.responseText);
-                        },
-                        success: function( oid ) {
-                            console.log(oid['status']);
-                            w2popup.open({
-                                title   : 'Descargar de acá',
-                                body    : 'Body HTML ' + oid['status'],
-                            });
-                            console.log('redirigiendo a ' + oid);
+                $('#download_form').w2form({ 
+                    name   : 'download_form',
+                    fields : [
+                        { name: 'first_name', type: 'text', required: true, html: {caption: 'Nombre'} },
+                        { name: 'last_name',  type: 'text', required: true, html: {caption: 'Apellidos' } },
+                        { name: 'email',  type: 'email', required: true, html: {caption: 'Correo Electrónico' } },
+                        { name: 'email_repeat',  type: 'email', required: true, html: {caption: 'Confirmar Correo' } },
+                        { name: 'org',  type: 'text', required: true, html: {caption: 'Institución' } },
+                        { name: 'comments',   type: 'textarea', html: {caption: 'Comentarios' }}
+                    ],
+                    onAction: function(event) {
+                        if (event['target'] == 'save') {
+                            if (this.record['email'] != this.record['email_repeat']) {
+                                this.error('Verifique su correo electrónico.');
+                                event.preventDefault();
+                            }
                         }
-                    });
+                    },
+                    actions: {
+                        reset: function () {
+                            this.clear();
+                        },
+                        save: function () {
+                            errors = this.validate(showErrors=true);
+                            if (errors.length==0) {
+                                this.lock();
+                                this.record['path'] = path
+                                $.ajax({
+                                    type: "POST",
+                                    url: "/register",
+                                    data: JSON.stringify( this.record ),
+                                    contentType: "application/json; charset=utf-8",
+                                    dataType: "json",
+                                    error: function(errMsg) {
+                                        this.error("AJAX error" + errMsg.responseText);
+                                    },
+                                    success: function( oid ) {
+                                        w2ui['download_form'].destroy();
+                                        w2popup.open({
+                                            title   : 'Popup Title HTML',
+                                            body    : 'Gracias. Liga de descarga: '+ oid['oid'],
+                                            width   : 400,
+                                            height  : 100
+                                        });
+                                    }});
+                            }
+                        }
+                    }
+                }); // form
+            } // on complete
+        }, // on open
+    }); // pop up
+}
 
-                } else {
-                    this.error('No coinciden las direcciones de correo electrónico provistas.a');
-                }
-            }
-        }
-
-    });
-});
